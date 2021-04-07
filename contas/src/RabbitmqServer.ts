775562c45a2f93ccaa5b2012d7d72ccd4f1cb99d
kdrs <kaiusdiego@gmail.com>
2021-04-07 00:40:25 -0300

    api de conta quase finalizada.

--- contas/src/RabbitmqServer.ts
+++ contas/src/RabbitmqServer.ts
@@ -1,18 +1,25 @@
 import { Connection, Channel, connect, Message } from "amqplib";
+import { config } from "dotenv"; 
+config({path:__dirname+'/./../../../.env'});
+
 
 export default class RabbitmqServer {
 
-    private static conn: Connection = null
+    private static conn: Connection
     private static channel: Channel = null
+    private exchange: string;
+    private queue: string;
+    private rkey: string;
 
-    constructor(private uri: string) { }
+    constructor(private uri: string = process.env.RABBITMQ_URI) { 
+    }
 
-    async init(): Promise<void> {
+    public async init(): Promise<Channel> {
         if (RabbitmqServer.conn == null) {
-            RabbitmqServer.conn = await connect(this.uri)
+            RabbitmqServer.conn = await connect(process.env.RABBITMQ_URI)
         }
         if (RabbitmqServer.channel == null) {
-            RabbitmqServer.channel = await RabbitmqServer.conn.createChannel()
+            return RabbitmqServer.channel = await RabbitmqServer.conn.createChannel()
         }
     }
 
@@ -20,8 +27,8 @@ export default class RabbitmqServer {
         RabbitmqServer.channel.assertExchange(exchange, type, { durable: true })
     }
 
-    async declareQueue(queue: string) {
-        RabbitmqServer.channel.assertQueue(queue, {
+    async declareQueue(queue: string): Promise<any> {
+        return RabbitmqServer.channel.assertQueue(queue, {
             durable: true
         })
     }
@@ -34,8 +41,12 @@ export default class RabbitmqServer {
         return RabbitmqServer.channel.sendToQueue(queue, Buffer.from(msg))
     }
 
-    async rKeyPublish(queue: string, rkey: string, msg: string) {
-        return RabbitmqServer.channel.publish(queue, rkey, Buffer.from(msg))
+    async rKeyPublish(exchange: string, rkey: string, msg: string) {
+        return RabbitmqServer.channel.publish(exchange, rkey, Buffer.from(msg))
+    }
+
+    async rKeyPublishQuick(msg: string) {
+        return RabbitmqServer.channel.publish(this.queue, this.rkey, Buffer.from(msg))
     }
 
     async consume(queue: string, callback: (msg: Message) => void) {
@@ -47,6 +58,20 @@ export default class RabbitmqServer {
 
     async closeConnection() {
         return RabbitmqServer.conn.close()
+    }
+
+    async configureAndDeclare(exchange: string, queue: string, rkey: string): Promise<void> {
+        this.exchange = exchange
+        this.queue = queue
+        this.rkey = rkey
+        await this.declareExchange(exchange).then(
+            await this.declareQueue.bind(null,queue)
+        )
+        await this.bindQueueExchange.bind(null,queue, exchange, rkey)
+    }
+
+    async getConnection(): Promise<Connection>{
+        return RabbitmqServer.conn;
     }
 
 }
--- contas/src/controllers/ContasController.ts
+++ contas/src/controllers/ContasController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from 'express'
-import AppController from './AppController'
-
 import  IConta  from "../models/IConta"
-
+import { config } from "dotenv"; 
 import { criarConta, obterSaldo, bloquearConta, obterTodas, depositarValor, sacarValor } from "../database/repositories/contaRepository";
+import RabbitmqServer from 'src/RabbitmqServer';
+config({path:__dirname+'/./../../../.env'});
 
-class ContasController extends AppController {
+class ContasController  {
 
+  
   /**
    *  index 
    * return lista de todas as contas
@@ -14,9 +15,9 @@ class ContasController extends AppController {
   public async index( req: Request, res: Response) {
     try {
       const all = await obterTodas()
-      return res.send(all)
+      return res.json(all)
     } catch (error) {
-      return res.send(`Não foi possível listar contas. ${error}`);
+      return res.json(`Não foi possível listar contas. ${error}`)
     }
     
 
@@ -29,9 +30,9 @@ class ContasController extends AppController {
   public async obterSaldo( req: Request, res: Response) {
     try {
       const saldo = await obterSaldo(Number.parseInt(req.params.id))
-      return res.send(saldo)
+      return res.json(saldo)
     } catch (error) {
-      return res.send(`Não foi possível obter saldo da conta. ${error}`);
+      return res.json(`Não foi possível obter saldo da conta. ${error}`)
     }
   }
 
@@ -48,9 +49,9 @@ class ContasController extends AppController {
   public async criar(req: Request, res: Response ) {
     try {
       const conta =  await criarConta(<IConta>req.body)
-      return res.send(conta)
+      return res.json(conta)
     } catch (error) {
-      return res.send(`Não foi possível criar a conta. ${error}`);
+      return res.json(`Não foi possível criar a conta. ${error}`)
     }
   }
 
@@ -58,13 +59,21 @@ class ContasController extends AppController {
    * put
    */
    public async depositar(req: Request, res: Response ) {
-    try {
+     try {
+      const server = new RabbitmqServer()
+      await server.init()
+      await server.declareExchange('contas')
+      await server.declareQueue('deposito')
+      await server.bindQueueExchange('deposito','contas','deposito.transacao')
+
       const valor = <number>req.body?.valor
       const idConta = Number.parseInt(req.params?.id)
       const conta =  await depositarValor(idConta,valor)
-      return res.send(conta)
+      const msgSent = await server.rKeyPublish('contas','deposito.transacao',
+      JSON.stringify({idConta,valor, dataTransacao: ''}))
+      return res.json({data:conta, msgSent})
     } catch (error) {
-      return res.send(`Não foi possível depositar valor. ${error}`);
+      return res.json(`Não foi possível depositar valor. ${error}`)
     }
   }
 
@@ -72,13 +81,21 @@ class ContasController extends AppController {
    * put
    */
    public async sacar(req: Request, res: Response ) {
-    try {
+     try {
+      const server = new RabbitmqServer()
+      await server.init()
+      await server.declareExchange('contas')
+      await server.declareQueue('saque')
+      await server.bindQueueExchange('saque','contas','saque.transacao')
+
       const valor = <number>req.body?.valor
       const idConta = Number.parseInt(req.params?.id)
       const conta =  await sacarValor(idConta,valor)
-      return res.send(conta)
+      const msgSent = await server.rKeyPublish('contas','saque.transacao',
+      JSON.stringify({idConta,valor, dataTransacao: ''}))
+      return res.json({data:conta})
     } catch (error) {
-      return res.send(`Não foi possível sacar valor. ${error}`);
+      return res.json(`Não foi possível sacar valor. ${error}`)
     }
   }
 
@@ -93,7 +110,7 @@ class ContasController extends AppController {
       return res.send(conta)
     } catch (error) {
       return res.send(`Não foi possível bloquear a conta. Verifique
-      se ela já não está bloqueada. ${error}`);
+      se ela já não está bloqueada. ${error}`)
     }
   }
 
